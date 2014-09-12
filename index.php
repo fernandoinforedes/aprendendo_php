992ca9a62e0afd473fa3c160598c1e2f9805346f
Fernando <fernando.inforedes@gmail.com>
2014-09-11 23:43:18 -0300

    alterado o arquivo index

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-
-echo "TEste Index";
+<html>
+    <head>
+        <title>
+            Página de Login Teste
+        </title>
+    </head>
+    <body>
+        <h1>Login de Acesso</h1>
+        <hr />
+        <table align="center" border="1">
+            <tr>
+                <th>
+                    <label>Usuário:</label>
+                </th>
+                <td>
+                    <input type="text" value="" name="user" />
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label>Senha:</label>
+                </th>
+                <td>
+                    <input type="text" value="" name="password" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                    <input type="submit" value="Entrar" />
+                </td>
+            </tr>
+        </table>
+</body>
+</html>
